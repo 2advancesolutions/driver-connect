81c8d431954e03bd7455bf3f405bb07c71e9a899
batman <2advancesolutions@gmail.com>
2023-01-18 21:22:39 -0600

    added login pages

---
+++ routes/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Text ,TextInput, Button, Paragraph } from 'react-native-paper';
+
+export function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+        // handle login
+    };
+
+    return (
+        <View>
+            <TextInput
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                autoCompleteType="email"
+            />
+            <TextInput
+                label="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={true}
+                autoCompleteType="password"
+            />
+            <Button mode="contained" onPress={handleSubmit}>
+                Login
+            </Button>
+            <Paragraph>Don't have an account? <Text>Sign up</Text> </Paragraph>
+            </View>
+    )
+}
